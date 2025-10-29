a900d672ae891628e24697d16dd3204f20136617
Jesus Anaya <31487994+laguna314@users.noreply.github.com>
2025-10-29 14:49:23 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RockwellEventCenterSite from './RockwellEventCenterSite.jsx';
+
+export default function App() {
+  return <RockwellEventCenterSite />;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: { accent: '#a6ff00' }
+    }
+  },
+  plugins: [],
+};
