cbf4c39a52fe78e1c9c921ee192afaac447287f5
Jesus Anaya <31487994+laguna314@users.noreply.github.com>
2026-01-29 00:59:55 -0600

    Delete worker.js

--- worker.js
+++
@@ -1,6 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    // If you later add routes/APIs, handle them here, otherwise let assets take over.
-    return env.ASSETS.fetch(request);
-  }
-}
